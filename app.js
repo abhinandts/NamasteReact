360b0b53eee384cb6849e3394e383ec60e2aa626
Abhinand T S <abhinandts116@gmail.com>
2025-01-08 11:19:04 +0530

    EP03

--- app.js
+++ app.js
@@ -1,67 +1,19 @@
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading" },
-//     "Hello World from React"
-// );
 
-// const parent = React.createElement(
-//     "div",
-//     { id: "div" },
-//     React.createElement(
-//         "div",
-//         { id: "div-inside-parent-div" },
-//         [
-//             React.createElement("h1", { id: heading }, "This is the H1....."),
-//             React.createElement("h2", {}, "This is H2...."),
-//             React.createElement("p", {}, "The first ever paragraph in react")
-//         ]
-//     )
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+
+// const HeadingComponent = () =>(
+//     <h1>Hello this is a functional component</h1>
 // )
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+const HeadingComponent = () => {
+    return (
+        <h2>New Heading of functional component</h2>
+    )
+}
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(parent);
-
-// console.log(parent);
-
-
-const heading = React.createElement("h1", {}, "This is the H1");
-
-const parent = React.createElement(
-    "div",
-    {},
-    [
-        React.createElement(
-            "div",
-            { id: "firstDiv" },
-
-            React.createElement(
-                "h1",
-                { id: "heading-inside-div1" },
-                "Heading..."
-            )
-        ),
-        React.createElement(
-            "div",
-            { id: "secondHeading" },
-            [React.createElement(
-                "h2",
-                {},
-                "h2 for the second div...."
-            ),
-            React.createElement(
-                "h3",
-                {},
-                "This is the second element in the 2nd div"
-            )
-            ]
-
-        )
-    ]
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
-console.log(parent);
+root.render(<HeadingComponent />);
