0393dbf507c372c6099668eb39b2d5ed7ae7ba9e
Abhinand T S <abhinandts116@gmail.com>
2025-01-11 08:21:55 +0530

    EP06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,49 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
 
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    const [filteredRes, setFilteredRes] = useState(resList);
+
+    useEffect(() => console.log("useEffect called"), []);
+
+    const [searchText, setSearchText] = useState("");
 
     return (
         <div className='body'>
             <div className='filter' >
+
+                <div className="search-div">
+                    <input type="text" className="search-box" value={searchText} onChange={(e) => { setSearchText(e.target.value) }} />
+
+                    <button onClick={() => {
+
+                        const filteredData = listOfRestaurants.filter(res => res.restaurantName.toLowerCase().includes(searchText.toLowerCase()))
+
+                        setFilteredRes(filteredData);
+
+                    }}>Search</button>
+
+                </div>
+
                 <button className="filter-btn"
                     onClick={() => {
-                        const filtered = listOfRestaurants.filter(res => res.starRating > 4.5);
-                        setListOfRestaurants(filtered);
+                        const filtered = filteredRes.filter(res => res.starRating > 4.5);
+                        setFilteredRes(filtered);
                     }}
                 >
                     Top Rated Restaurants
                 </button>
+
             </div>
             <div className='res-container'>
 
                 {
-                    listOfRestaurants.map((restaurant, index) => <RestaurantCard key={index} resData={restaurant} />)
+                    filteredRes.map((restaurant, index) => <RestaurantCard key={index} resData={restaurant} />)
                 }
 
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 
 import { LOGO_URL } from "../utils/constants";
 
+import { useState } from "react";
+
 const Header = () => {
+
+    let btnName = "Log In";
+
+    let [superBtnName,setSuperBtnName] = useState("Log In");
+
     return (
         <div className='header'>
             <div className='logo-container'>
-                <img className='logo' src= {LOGO_URL} />
+                <img className='logo' src={LOGO_URL} />
             </div>
 
             <div className='nav-links'>
@@ -14,6 +21,14 @@ const Header = () => {
                     <li>Contact Us</li>
                     <li>About Us</li>
                     <li>Cart</li>
+                    <li>
+                        <button className="login"
+                            onClick={() => {
+                                setSuperBtnName("Log Out");
+                                console.log(superBtnName);
+                            }}
+                        >{superBtnName}</button>
+                    </li>
                 </ul>
             </div>
         </div>
