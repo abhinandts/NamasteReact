3097761fbadeb95088f95958326e9fe4066e9ab2
Abhinand T S <abhinandts116@gmail.com>
2025-01-24 09:54:02 +0530

    checking commit on new branch

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,9 +6,10 @@ const Body = () => {
     return (
         <div className='body'>
             <div className='search' >
-                <h3>Search</h3>
+                <h3>find</h3>
             </div>
             <div className='res-container'>
+
 
                 {
                     resList.map((restaurant,index) => <RestaurantCard key={index} resData={restaurant} />)
